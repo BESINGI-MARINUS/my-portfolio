e6dd2b932b889438845dccff4ef00f23c86fb4cc
BESINGI MARINUS <marinusnyando@gmail.com>
2025-06-25 10:40:37 +0100

    Made the nav-bar responsive

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// ELEMENTS
+const toggleInput = document.getElementById('toggle-input');
+const sideNav = document.querySelector('.side__items');
+
+toggleInput.addEventListener('change', function (e) {
+  console.log('Exe', e.target);
+  sideNav.classList.toggle('hide-nav');
+  sideNav.style.transition = 'transform .3s';
+});
