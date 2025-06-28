efa028846df75e37ac4c75bee4f9a6983fa1ddad
BESINGI MARINUS <marinusnyando@gmail.com>
2025-06-28 04:54:37 +0100

    Fix some media query issues

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,7 +5,6 @@ const toggleInput = document.getElementById('toggle-input');
 const sideNav = document.querySelector('.side__items');
 
 toggleInput.addEventListener('change', function (e) {
-  console.log('Exe', e.target);
   sideNav.classList.toggle('hide-nav');
   sideNav.style.transition = 'transform .3s';
 });
