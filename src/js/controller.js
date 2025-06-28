922999bfbf4760836473208d3137e9cb9fe2be48
BESINGI MARINUS <marinusnyando@gmail.com>
2025-06-28 10:26:41 +0100

    Added smooth scrolling and sticky nav

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,8 +3,43 @@
 // ELEMENTS
 const toggleInput = document.getElementById('toggle-input');
 const sideNav = document.querySelector('.side__items');
+const sections = document.querySelectorAll('.section');
 
+// Hamburger menu
 toggleInput.addEventListener('change', function (e) {
   sideNav.classList.toggle('hide-nav');
   sideNav.style.transition = 'transform .3s';
 });
+
+// Smooth scrolling
+const desktopNav = document.querySelector('.nav__items');
+
+desktopNav.addEventListener('click', function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute('href');
+  if (!id) return;
+  document.querySelector(`${id}`).scrollIntoView({ behavior: 'smooth' });
+});
+
+// Sticky Nav
+const heroSection = document.querySelector('.section-hero');
+const nav = document.querySelector('.nav');
+const navHeight = getComputedStyle(nav).height;
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        nav.classList.add('sticky');
+      } else {
+        nav.classList.remove('sticky');
+      }
+    });
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}`,
+  }
+);
+observer.observe(heroSection);
